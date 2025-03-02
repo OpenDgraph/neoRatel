37339d5c7ea751802fadcbec341a746c4e26745c
Michel Diz <MichelDiz@users.noreply.github.com>
2025-03-02 18:47:30 -0300

    refactor: enhance dgraphConfigStore with persistence and additional state management

--- src/store/dgraphConfigStore.ts
+++ src/store/dgraphConfigStore.ts
@@ -1,31 +1,51 @@
-import create from 'zustand';
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
-type State = {
-  clusterUrl: string,
-  serverQueryTimeout: number,
-  slashApiKey: string,
-  authToken: string,
-  aclToken: string,
-  dialogState: boolean,
-  setDialogState: (state: boolean) => void,
-  setAclToken: (tokenACL: string) => void,
-  setclusterUrl: (url: string) => void,
-  setServerQueryTimeout: (timeout: number) => void,
-  setSlashApiKey: (key: string) => void,
-  setAuthToken: (token: string) => void,
-};
+interface State {
+  clusterUrl: string;
+  serverQueryTimeout: number;
+  slashApiKey: string;
+  authToken: string;
+  aclToken: string;
+  dialogState: boolean;
+  isConfigured: boolean;
+  setDialogState: (state: boolean) => void;
+  setAclToken: (tokenACL: string) => void;
+  setclusterUrl: (url: string) => void;
+  setServerQueryTimeout: (timeout: number) => void;
+  setSlashApiKey: (key: string) => void;
+  setAuthToken: (token: string) => void;
+  setIsConfigured: (state: boolean) => void;
+}
 
-export const useDgraphConfigStore = create<State> ((set) =>  ({
-  clusterUrl: 'https://play.dgraph.io',
-  serverQueryTimeout: 20,
-  slashApiKey: '', 
-  authToken: '',
-  aclToken: '',
-  dialogState: true,
-  setDialogState: (state) => set({ dialogState: state }),
-  setAclToken: (tokenACL) => set({ aclToken: tokenACL }),
-  setclusterUrl: (url) => set({ clusterUrl: url }),
-  setServerQueryTimeout: (timeout) => set({ serverQueryTimeout: timeout }),
-  setSlashApiKey: (key) => set({ slashApiKey: key }),
-  setAuthToken: (token) => set({ authToken: token }),
-}));
+export const useDgraphConfigStore = create<State>()(
+  persist(
+    (set) => ({
+      clusterUrl: 'https://play.dgraph.io',
+      serverQueryTimeout: 20,
+      slashApiKey: '',
+      authToken: '',
+      aclToken: '',
+      dialogState: false,
+      isConfigured: false,
+      setDialogState: (state) => set({ dialogState: state }),
+      setAclToken: (tokenACL) => set({ aclToken: tokenACL }),
+      setclusterUrl: (url) => set({ clusterUrl: url }),
+      setServerQueryTimeout: (timeout) => set({ serverQueryTimeout: timeout }),
+      setSlashApiKey: (key) => set({ slashApiKey: key }),
+      setAuthToken: (token) => set({ authToken: token }),
+      setIsConfigured: (state) => set({ isConfigured: state }),
+    }),
+    {
+      name: 'dgraph-config-storage',
+      partialize: (state) => ({
+        clusterUrl: state.clusterUrl,
+        serverQueryTimeout: state.serverQueryTimeout,
+        slashApiKey: state.slashApiKey,
+        authToken: state.authToken,
+        aclToken: state.aclToken,
+        isConfigured: state.isConfigured,
+      }),
+    }
+  )
+);
